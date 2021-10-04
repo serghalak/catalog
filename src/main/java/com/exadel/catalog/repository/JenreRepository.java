58fa8c54f02622a9f72fbd3852e4b5aefda8aa8b
serghalak <serghalak@gmail.com>
2021-10-04 14:13:02 +0300

    add crud repositories

---
+++ src/main/java/com/exadel/catalog/repository/JenreRepository.java
@@ -0,0 +1,7 @@
+package com.exadel.catalog.repository;
+
+import com.exadel.catalog.domain.Jenre;
+import org.springframework.data.repository.CrudRepository;
+
+public interface JenreRepository extends CrudRepository<Jenre, Long> {
+}
