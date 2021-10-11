f48ffb91dce20f8c5e0dd29df0b4460f32129f12
serghalak <serghalak@gmail.com>
2021-10-11 14:34:50 +0300

    add Jenre (controller, service)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,11 @@ INSERT INTO  AUTHOR (FIRST_NAME, LAST_NAME) VALUES
 INSERT INTO  PUBLISHER (NAME, ADDRESS) VALUES
 ( 'PROGRAMMER', 'LVIV франка 30'),
 ( 'IT Today','Iv-Frankivsk Mazepa 145');
+
+INSERT INTO  JENRE (NAME) VALUES
+( 'WEB'),
+( 'Game'),
+('Administrating'),
+('System'),
+('OS'),
+('LAN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,3 @@
 create table author (id bigint not null AUTO_INCREMENT, first_name varchar(255), last_name varchar(255), primary key (id));
 create table publisher (id bigint not null AUTO_INCREMENT, name varchar(255), address varchar(255), primary key (id));
+create table jenre (id bigint not null AUTO_INCREMENT, name varchar(255), primary key (id));
